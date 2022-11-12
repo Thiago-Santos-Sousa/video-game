c0eaf0cb994bb8689e894e9c13942d92618fda8d
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-11-12 18:01:37 -0300

    Atualizando rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -9,13 +9,13 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'sarch/:game-search',
+    path: 'search/:game-search',
     component: HomeComponent,
   },
   {
     path: 'details/:id',
     component: DetailsComponent,
-  }
+  },
 ];
 
 @NgModule({
