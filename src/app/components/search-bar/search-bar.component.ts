e80e291939709d0541ae8a667c019c0035af1d07
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-11-12 17:54:42 -0300

    Atualizando final do arquivo

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -14,7 +14,8 @@ export class SearchBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(form: NgForm){
-    this.router.navigate(['search', form.value.search])
+  onSubmit(form: NgForm) {
+    this.router.navigate(['search', form.value.search]);
   }
+
 }
