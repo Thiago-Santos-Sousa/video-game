9e02e507ff1e76c775b867a5d683a5eb629df2ea
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-11-12 18:12:01 -0300

    FInalizando interceptor-error

--- src/app/interceptors/http-errors.interceptor.ts
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -1,20 +1,27 @@
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError as observableThrowError} from 'rxjs';
-import { catchError} from 'rxjs/operators';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
 @Injectable()
 export class HttpErrorsInterceptor implements HttpInterceptor {
-  constructor() {}
+constructor() {}
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      catchError((err) => {
-        console.log(err);
-        return observableThrowError(err);
-      })
-    );
-  }
+intercept(
+  req: HttpRequest<any>,
+  next: HttpHandler
+): Observable<HttpEvent<any>> {
+  return next.handle(req).pipe(
+    catchError((err) => {
+      console.log(err);
+      return observableThrowError(err);
+    })
+  );
+}
 }
